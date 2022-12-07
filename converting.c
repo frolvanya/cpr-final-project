2b2396d10cc7b05ef098f9450e76307c971835da
Ivan Frolov <frolvanya@gmail.com>
2022-12-06 21:37:52 -0500

    style: removed comments

--- converting.c
+++ converting.c
@@ -10,32 +10,32 @@
 void converting(void)
 {
     // V1
-    // printf("*** Start of Converting Strings to int Demo ***\n");
+    printf("*** Start of Converting Strings to int Demo ***\n");
 
-    // // declaring cstring variable with length of BUFFER_SIZE
-    // char intString[BUFFER_SIZE];
+    // declaring cstring variable with length of BUFFER_SIZE
+    char intString[BUFFER_SIZE];
 
-    // // declaring int variable
-    // int intNumber;
+    // declaring int variable
+    int intNumber;
 
-    // do {
-    //     printf("Type an int numeric string (q - to quit):\n");
+    do {
+        printf("Type an int numeric string (q - to quit):\n");
 
-    //     // Read string from stdin, stops when BUFFER_SIZE - 1 characters are read or \n is reached and stores to intString
-    //     fgets(intString, BUFFER_SIZE, stdin);
+        // Read string from stdin, stops when BUFFER_SIZE - 1 characters are read or \n is reached and stores to intString
+        fgets(intString, BUFFER_SIZE, stdin);
 
-    //     // Setting the last character in cstring to null delimiter byte
-    //     intString[strlen(intString) - 1] = '\0';
+        // Setting the last character in cstring to null delimiter byte
+        intString[strlen(intString) - 1] = '\0';
 
-    //     // if intString != "q"
-    //     if (strcmp(intString, "q") != 0) {
-    //         // string to int convertation
-    //         intNumber = atoi(intString);
-    //         printf("Converted number is %d\n", intNumber);
-    //     }
-    //     // while (intString != "q")
-    // } while (strcmp(intString, "q") != 0);
-    // printf("*** End of Converting Strings to int Demo ***\n\n");
+        // if intString != "q"
+        if (strcmp(intString, "q") != 0) {
+            // string to int convertation
+            intNumber = atoi(intString);
+            printf("Converted number is %d\n", intNumber);
+        }
+        // while (intString != "q")
+    } while (strcmp(intString, "q") != 0);
+    printf("*** End of Converting Strings to int Demo ***\n\n");
 
     // V2
     printf("*** Start of Converting Strings to double Demo ***\n");
